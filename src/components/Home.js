25360f87e3134b392016b548f250cc569508aaf1
Emily Daykin <emilydaykin31@gmail.com>
2022-05-17 16:43:49 +0200

    refactored Home to BEM

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,20 +17,21 @@ const Home = () => {
   console.log('all series:', series);
 
   return (
-    <section>
+    <section className='home'>
       <h1>
         <span>ISDb</span> Internet Series Database
       </h1>
-
       {series ? (
-        <div className='catalogue'>
+        <div className='home__catalogue'>
           {series.map((show) => (
-            <div key={show._id} className='series-card'>
-              <Link className='link' to={`/series/${show._id}`}>
+            <div key={show._id} className='home__series-card'>
+              <Link className='home__series-link' to={`/series/${show._id}`}>
                 <h2>{show.name}</h2>
-                {/* <figure> */}
-                <img className='poster' src={show.image} alt={`${show.name} series poster`} />
-                {/* </figure> */}
+                <img
+                  className='home__series-poster'
+                  src={show.image}
+                  alt={`${show.name} series poster`}
+                />
               </Link>
               <p>{show.description}</p>
             </div>
