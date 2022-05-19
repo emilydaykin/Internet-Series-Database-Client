671226dd717f8c7dbf05df9c80eac5a3f98e0625
Emily Daykin <emilydaykin31@gmail.com>
2022-05-19 23:59:13 +0200

    home page styling to search and filter control additions

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,16 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   const [series, setSeries] = useState(null);
+  const [uniqueGenres, setUniqueGenres] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
       const allSeries = await getAllSeries();
+      console.log('all series:', allSeries);
       setSeries(allSeries);
+      const allGenres = allSeries.map((series) => series.genre);
+      console.log('all genres:', allGenres.flat());
+      setUniqueGenres([...new Set(allGenres.flat())]);
     };
     getData();
 
@@ -19,32 +24,52 @@ const Home = () => {
     // filterSeries();
   }, []);
 
-  console.log('all series:', series);
+  console.log('uniqueGenres:', uniqueGenres);
 
   return (
     <section className='home'>
       <h1 className='u-margin-bottom-small'>
         <span>ISDb</span> Internet Series Database
       </h1>
-      {series ? (
-        <div className='home__catalogue'>
-          {series.map((show) => (
-            <div key={show._id} className='home__series-card'>
-              <Link className='home__series-link' to={`/series/${show._id}`}>
-                <h2>{show.name}</h2>
-                <img
-                  className='home__series-poster'
-                  src={show.image}
-                  alt={`${show.name} series poster`}
-                />
-              </Link>
-              <p>{show.description}</p>
+      <div className='home__content'>
+        <div className='home__controls'>
+          <input
+            type='text'
+            className='home__search-bar'
+            placeholder='Search (title, actor, plot, year, rating, genre)'
+          />
+          {uniqueGenres.length !== 0 ? (
+            <div className='home__filters'>
+              {uniqueGenres.map((genre) => (
+                <div key={genre} className='home__filter'>
+                  {genre}
+                </div>
+              ))}
             </div>
-          ))}
+          ) : (
+            <p></p>
+          )}
         </div>
-      ) : (
-        <p>Loading series...</p>
-      )}
+        {series ? (
+          <div className='home__catalogue'>
+            {series.map((show) => (
+              <div key={show._id} className='home__series-card'>
+                <Link className='home__series-link' to={`/series/${show._id}`}>
+                  <h2>{show.name}</h2>
+                  <img
+                    className='home__series-poster'
+                    src={show.image}
+                    alt={`${show.name} series poster`}
+                  />
+                </Link>
+                <p>{show.description}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Loading series...</p>
+        )}
+      </div>
     </section>
   );
 };
