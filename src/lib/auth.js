fa66b4d7fce68b9dadf31a540b57cd8fa80b6f4e
Emily Daykin <emilydaykin31@gmail.com>
2022-05-21 11:36:12 +0200

    return userObject (rather than just id) from getting loggedIn user, and cleaned up lib/auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,13 +1,3 @@
-// Different functions for working with my token
-
-// function checkSessionStorageToken() {
-//   const token = sessionStorage.getItem('token');
-//   if (!token) return false; // safely in case there's no token
-
-//   const userObject = JSON.parse(window.atob(token.split('.')[1]));
-//   return userObject;
-// }
-
 // Getting the loggedin-user's ID
 export function getLoggedInUser() {
   const token = sessionStorage.getItem('token');
@@ -15,9 +5,7 @@ export function getLoggedInUser() {
     return false; // safely in case there's no token
   } else {
     const userObject = JSON.parse(window.atob(token.split('.')[1]));
-    // console.log('userObject.userId', userObject.userId);
-    // console.log('Logged in user object', userObject);
-    return userObject.userId;
+    return userObject;
   }
 }
 
@@ -28,9 +16,6 @@ export function isAdmin() {
     return false; // safely in case there's no token
   } else {
     const userObject = JSON.parse(window.atob(token.split('.')[1]));
-    // console.log({ token });
-    // console.log('userObject Admin', userObject);
-    // console.log('return', !!userObject.isAdmin);
     return !!userObject.isAdmin; // protects from undefined
   }
 }
