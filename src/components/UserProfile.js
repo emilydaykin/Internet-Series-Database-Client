19f2aa167a76d8da3ab4a0a60314c4dd7b67d188
Emily Daykin <emilydaykin31@gmail.com>
2022-05-21 14:35:45 +0200

    added series recommendation logic to user-profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -5,13 +5,17 @@ import { getAllSeries } from '../api/series';
 
 const UserProfile = () => {
   const userObject = getLoggedInUser();
-  console.log('userObject', userObject);
-  const [favourites, setFavourites] = useState(null);
 
+  const [allSeries, setAllSeries] = useState(null);
+  const [favourites, setFavourites] = useState(null);
+  const [recommendations, setRecommendations] = useState(null);
+
+  // TEMP
   useEffect(() => {
     const getData = async () => {
       const allSeries = await getAllSeries();
-      setFavourites(allSeries.slice(20, 30));
+      setAllSeries(allSeries);
+      setFavourites(allSeries.slice(10, 30));
     };
     getData();
   }, []);
@@ -21,32 +25,53 @@ const UserProfile = () => {
   const calculateFavouriteGenre = () => {
     if (favourites) {
       const allLikedGenres = favourites.map((series) => series.genre).flat();
-      console.log('allLikedGenres', allLikedGenres);
+      // console.log('allLikedGenres', allLikedGenres);
       const genreCount = {};
       allLikedGenres.forEach(
         (genre) => (genreCount[genre] = genreCount[genre] ? genreCount[genre] + 1 : 1)
       );
-      console.log('genreCount', genreCount);
+      // console.log('genreCount', genreCount);
       const maxGenreOccurance = Math.max(...Object.values(genreCount));
       const favouriteGenre = Object.keys(genreCount).find(
         (key) => genreCount[key] === maxGenreOccurance
       );
-      console.log('favouriteGenre', favouriteGenre, `(${maxGenreOccurance})`);
+      // console.log('favouriteGenre', favouriteGenre, `(${maxGenreOccurance})`);
+      return favouriteGenre;
     }
   };
 
-  calculateFavouriteGenre();
+  const getRecommendations = () => {
+    if (allSeries && favourites) {
+      // 'similaries' = modal genre in their 'liked' list
+      console.log('fave genre:', calculateFavouriteGenre());
+      const nonLikedSimilaries = allSeries.filter(
+        (series) =>
+          series.genre.includes(calculateFavouriteGenre()) &&
+          !favourites.map((likedShow) => likedShow._id).includes(series._id)
+      );
+      console.log('nonLikedSimilaries', nonLikedSimilaries);
+      // Shorten the recommendation list by picking 12 random similar series:
+      const shuffledList = [...nonLikedSimilaries].sort(() => 0.5 - Math.random());
+      return shuffledList.slice(0, 12); // will return all if length < 12.
+    }
+  };
+
+  getRecommendations();
 
   return (
     <section className='user-profile'>
       <h1 className='user-profile__welcome'>Hello {userObject.userName}!</h1>
       <div className='user-profile__favourites'>
         <h2>Favourite Series {favourites ? `(${favourites.length})` : ''}</h2>
-        <p className='u-margin-top-small'>The list of series that you've 'liked'.</p>
-        <ElasticCarousel faves={favourites} />
+        <p className='user-profile__text u-margin-top-small'>
+          The list of {favourites ? `${favourites.length}` : ''} series that you have 'liked'.
+        </p>
+        <ElasticCarousel seriesList={favourites} />
       </div>
-      <div className='user-profile__favourites'>
-        <h2>Recommended Series For You</h2>
+      <div className='user-profile__recommendations'>
+        <h2>Other Series You May Like</h2>
+        <p className='user-profile__text u-margin-top-small'>Based on your 'liked' series.</p>
+        <ElasticCarousel seriesList={getRecommendations()} />
       </div>
     </section>
   );
