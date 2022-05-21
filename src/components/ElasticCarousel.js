a9586c9dc896034302a954ee42b0ed955532a4f5
Emily Daykin <emilydaykin31@gmail.com>
2022-05-21 12:18:48 +0200

    added actors to back of carousel card

--- src/components/ElasticCarousel.js
+++ src/components/ElasticCarousel.js
@@ -3,20 +3,18 @@ import Carousel from 'react-elastic-carousel';
 import { Link } from 'react-router-dom';
 
 const ElasticCarousel = ({ faves }) => {
-  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
-
   const responsiveBreakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 2, itemsToScroll: 2 },
     { width: 768, itemsToShow: 3 },
     { width: 1200, itemsToShow: 4 }
   ];
+
   return (
     <div className='carousel'>
       {faves ? (
         <Carousel breakPoints={responsiveBreakPoints}>
           {faves.map((series) => (
-            // <Item key={item}>{item}</Item>
             <Link key={series._id} to={`/series/${series._id}`} className='card'>
               <div className='card__side card__side--front'>
                 <img src={series.image} alt={series.name} className='carousel__image' />
@@ -27,15 +25,12 @@ const ElasticCarousel = ({ faves }) => {
                   ({series.pilotYear}&#8212;{series.finaleYear})
                 </p>
                 <p className='card__details card__details--rating'>⭐️ {series.rating} ⭐️</p>
-                <p className='card__details card__details--genres'>{series.genre.join(', ')}</p>
+                <p className='card__details card__details--genres'>{series.genre.join(' • ')}</p>
                 <p className='card__details card__details--actors'>
-                  {series.actors.forEach((actor) => {
-                    console.log('actor', actor);
-                    return <span key={actor}>actor</span>;
-                    // return actor;
-                  })}
+                  {series.actors.map((actor) => (
+                    <span key={actor}>{actor}</span>
+                  ))}
                 </p>
-                {/* <p className='card__details card__details--actors'>{series.actors.join('<br/>')}</p> */}
               </div>
             </Link>
           ))}
