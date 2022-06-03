2d235463140067e4eb31dd44805173b6c1819fcf
Emily Daykin <emilydaykin31@gmail.com>
2022-06-03 10:44:29 +0200

    first (login) integration test complete

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -2,6 +2,12 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Login from '../components/auth/Login';
+import userEvent from '@testing-library/user-event';
+
+const mockUser = {
+  email: 'mock@user.com',
+  password: 'mockPassword1!'
+};
 
 test('Accept user input and display it correctly', () => {
   // Login (since it contains useNavigate) must be wrapped in BrowserRouter
@@ -11,8 +17,15 @@ test('Accept user input and display it correctly', () => {
     </BrowserRouter>
   );
 
+  // Get the login form's email input element:
   const emailInput = screen.getByLabelText('Email');
 
   // .toBeInTheDocument() comes from `jest-dom`
   expect(emailInput).toBeInTheDocument();
+
+  // simulate user typing in email to form's email field:
+  userEvent.type(emailInput, mockUser.email);
+
+  // emailInput.value is from emailInput.pendingProps.value
+  expect(emailInput.value).toEqual(mockUser.email);
 });
