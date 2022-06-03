b8ff57e2512c1456a5100840586269242c25d86a
Emily Daykin <emilydaykin31@gmail.com>
2022-06-03 18:33:57 +0200

    Added 4th unit test for login to assert no error messages upon initial render

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -10,7 +10,7 @@ const mockUser = {
   password: 'mockPassword1!'
 };
 
-test('Accept user input and display it correctly', () => {
+test('Assert user inputs are accepted and displayed correctly', () => {
   // Login (since it contains useNavigate) must be wrapped in BrowserRouter
   render(
     <BrowserRouter>
@@ -39,31 +39,11 @@ test('Accept user input and display it correctly', () => {
   expect(passwordInput.value).toEqual(mockUser.password);
 });
 
-test('Upon submit, post input values correctly to API', async () => {
+test('Assert input values are correctly sent as a post request to API upon submit', async () => {
   // Initialise the value of axios.post by creating a mock
   axios.post = jest.fn();
-  // axios.post.mockResolvedValueOnce();
   axios.post();
-  // const postRequest = axios.post();
-  // console.log('axios.post.mock 1-----------------', axios.post.mock);
-  // console.log('axios.post.mock.instances', axios.post.mock.instances);
-
-  // const axiosMock = axios;
-  // axiosMock.create = jest.fn();
-  // axiosMock.create.mockReturnValue(axiosMock);
-  // axiosMock.post.mockResolvedValue(mockUser);
-
   // axios.post.mockResolvedValueOnce();
-  // axios.post.mockResolvedValue(mockUser);
-
-  // expect(axios.post).toHaveBeenCalledWith();
-
-  // const mockAxiosPost = jest.fn();
-
-  // mockAxiosPost();
-
-  // expect(mockAxiosPost).toHaveBeenCalledTimes(1);
-  // expect(mockAxiosPost).toHaveBeenCalledWith();
 
   render(
     <BrowserRouter>
@@ -80,26 +60,61 @@ test('Upon submit, post input values correctly to API', async () => {
   userEvent.type(passwordInput, mockUser.password);
   userEvent.click(submitButton);
 
-  // axios.post.mockResolvedValueOnce();
+  await waitFor(() => expect(axios.post).toHaveBeenCalled());
+  await waitFor(() => expect(axios.post).toBeCalledTimes(1));
+
+  // expect(axios.post).toBeCalledWith('http://localhost:8001/api/login', mockUser);
+  // expect(axios.post).toHaveBeenCalledWith('http://localhost:8001/api/login', mockUser);
+});
+
+test('Assert error message displayed when incorrect credentials are provided', async () => {
+  // Initialise the value of axios.post by creating a mock
+  axios.post = jest.fn();
+  axios.post();
+  // axios.post.mockRejectedValueOnce();
+
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );
+
+  const emailInput = screen.getByLabelText('Email');
+  const passwordInput = screen.getByLabelText('Password');
+  const submitButton = screen.getByRole('button', { className: /button/i });
+
+  userEvent.type(emailInput, mockUser.email);
+  userEvent.type(passwordInput, mockUser.password);
+  userEvent.click(submitButton);
 
   await waitFor(() => expect(axios.post).toHaveBeenCalled());
-  // await waitFor(() => expect(mockAxiosPost).toHaveBeenCalled());
-  await waitFor(() => expect(axios.post).toBeCalledTimes(1));
-  // await waitFor(() => expect(mockAxiosPost).toBeCalledTimes(1));
-  // console.log('axios.post', axios.post());
-  // console.log('postRequest', postRequest);
-  // expect(mockAxiosPost).toBeCalledWith('http://localhost:8002/login', mockUser);
 
-  // console.log('axios.post.mock 2 ------------', axios.post.mock);
-  // expect(axios.post).toBeCalledWith('http://localhost:8001/api/login', mockUser);
+  const errorMessage = screen.getByRole('error-message', { className: /form__error-message/i });
+  console.log('errorMessage', errorMessage);
 
-  // expect(axios.request).toHaveBeenCalledWith({
-  //   method: 'POST',
-  //   url: 'http://localhost:8001/api/login',
-  //   data: mockUser
-  // });
+  expect(errorMessage).toBeInTheDocument();
+  expect(errorMessage).toHaveTextContent('Incorrect credentials.');
+});
 
-  // await waitFor(() =>
-  //   expect(axios.post).toBeCalledWith('http://localhost:8001/api/login', mockUser)
-  // );
+test('Assert that no error message is displayed upon initial render', () => {
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );
+
+  // Get text of all possible error messages:
+  const errorMessageFormNotFilled = screen.queryByText('Please enter your credentials.');
+  const errorMessageEmailNotFilled = screen.queryByText('Please enter your email.');
+  const errorMessagePasswordNotFilled = screen.queryByText('Please enter your password.');
+  const errorMessageIncorrectCreds = screen.queryByText('Incorrect credentials.');
+
+  const possibleErrorMessages = [
+    errorMessageFormNotFilled,
+    errorMessageEmailNotFilled,
+    errorMessagePasswordNotFilled,
+    errorMessageIncorrectCreds
+  ];
+
+  possibleErrorMessages.forEach((errMsg) => expect(errMsg).not.toBeInTheDocument());
 });
