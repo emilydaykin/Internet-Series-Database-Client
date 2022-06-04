63e3f31a480f6301b12863a6c2c7b89592025e87
Emily Daykin <emilydaykin31@gmail.com>
2022-06-04 12:37:00 +0200

    3rd unit test for register successful

--- src/__tests__/Register.test.js
+++ src/__tests__/Register.test.js
@@ -1,15 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Register from '../components/Register';
 import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 
 const mockUser = {
-  name: 'mcMock',
+  username: 'mcMock',
   email: 'mock@user.com',
   password: 'mockPassword1!',
   passwordConfirmation: 'mockPassword1!'
 };
+
+afterEach(cleanup);
 
 test('Assert no error messages are displayed upon initial render', () => {
   render(
@@ -56,13 +59,39 @@ test('Assert user inputs are accepted and displayed correctly', () => {
   expect(passwordConfirmationInput).toBeInTheDocument();
   expect(submitButton).toBeInTheDocument();
 
-  userEvent.type(nameInput, mockUser.name);
+  userEvent.type(nameInput, mockUser.username);
   userEvent.type(emailInput, mockUser.email);
   userEvent.type(passwordInput, mockUser.password);
   userEvent.type(passwordConfirmationInput, mockUser.passwordConfirmation);
 
-  expect(nameInput.value).toEqual(mockUser.name);
+  expect(nameInput.value).toEqual(mockUser.username);
   expect(emailInput.value).toEqual(mockUser.email);
   expect(passwordInput.value).toEqual(mockUser.password);
   expect(passwordConfirmationInput.value).toEqual(mockUser.passwordConfirmation);
+});
+
+test('Assert input values are correctly sent as a post request to API upon submit', async () => {
+  axios.post = jest.fn();
+  axios.post();
+
+  render(
+    <BrowserRouter>
+      <Register />
+    </BrowserRouter>
+  );
+
+  const nameInput = screen.getByLabelText('Name');
+  const emailInput = screen.getByLabelText('Email');
+  const passwordInput = screen.getByLabelText('Password');
+  const passwordConfirmationInput = screen.getByLabelText('Password Confirmation');
+  const submitButton = screen.getByRole('button', { className: /button/i });
+
+  userEvent.type(nameInput, mockUser.username);
+  userEvent.type(emailInput, mockUser.email);
+  userEvent.type(passwordInput, mockUser.password);
+  userEvent.type(passwordConfirmationInput, mockUser.passwordConfirmation);
+  userEvent.click(submitButton);
+
+  await waitFor(() => expect(axios.post).toHaveBeenCalled());
+  await waitFor(() => expect(axios.post).toBeCalledTimes(1));
 });
