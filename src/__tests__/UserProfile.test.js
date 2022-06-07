e21870769123feda7f6846fe851ccc3e8f6a4725
Emily Daykin <emilydaykin31@gmail.com>
2022-06-07 22:35:21 +0200

    added 2 integration tests to UserProfile to test its interaction with ElasticCarousel

--- src/__tests__/UserProfile.test.js
+++ src/__tests__/UserProfile.test.js
@@ -1,10 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserProfile from '../components/UserProfile';
+import axios from 'axios';
+import { BrowserRouter } from 'react-router-dom';
+import { mockSeriesData } from '../__mocks__/seriesData';
 
-// Only the elastic carousel depends on the (mock) data, so that's
-// tested there, and no need for an `act(...)` wrapper here since
-// we're testing the initial state only.
+// ---------- Unit Tests ---------- //
 
 test('Assert user greeting is displayed', () => {
   render(<UserProfile />);
@@ -14,16 +15,70 @@ test('Assert user greeting is displayed', () => {
   expect(greeting).toHaveTextContent('Hello');
 });
 
-test('Assert favourited series div to be displayed', async () => {
+test('Assert favourited series div to be displayed', () => {
   render(<UserProfile />);
 
   const favouriteSeriesHeading = screen.queryByText(/favourite series/i);
   expect(favouriteSeriesHeading).toBeInTheDocument();
 });
 
-test('Assert recommended series div to be displayed', async () => {
+test('Assert recommended series div to be displayed', () => {
   render(<UserProfile />);
 
   const recommendationsHeading = screen.queryByText(/other series you may like/i);
   expect(recommendationsHeading).toBeInTheDocument();
+});
+
+// ---------- Integration Tests ---------- //
+// Testing to see how successfully the UserProfile component calls
+// and passes props to the ElasticCarousel component inside it
+
+test('(INT) Assert no carousel displayed if user has no favourited series', async () => {
+  axios.get = jest.fn();
+  axios.get.mockResolvedValueOnce({ data: [] });
+
+  await act(async () => {
+    render(
+      <BrowserRouter>
+        <UserProfile />
+      </BrowserRouter>
+    );
+  });
+
+  await waitFor(() => expect(axios.get).toHaveBeenCalled());
+
+  const NoFavouritesMessage = screen.queryByText(/no favourites yet/i);
+  expect(NoFavouritesMessage).toBeInTheDocument();
+  const NoRecommendationsMessage = screen.queryByText(/no recommendations yet/i);
+  expect(NoRecommendationsMessage).toBeInTheDocument();
+
+  const carousel = screen.queryByText(/breakPoints={responsiveBreakPoints}/i);
+  const card = screen.queryByText("className='card__side card__side--front'");
+  console.log('card', card);
+  expect(carousel).not.toBeInTheDocument();
+  expect(card).not.toBeInTheDocument();
+});
+
+test('(INT) Assert carousels are displayed if user has favourited series', async () => {
+  axios.get = jest.fn();
+  axios.get.mockResolvedValueOnce({ data: mockSeriesData });
+
+  await act(async () => {
+    render(
+      <BrowserRouter>
+        <UserProfile />
+      </BrowserRouter>
+    );
+  });
+
+  await waitFor(() => expect(axios.get).toHaveBeenCalled());
+
+  const NoFavouritesMessage = screen.queryByText(/no favourites yet/i);
+  expect(NoFavouritesMessage).not.toBeInTheDocument();
+  const NoRecommendationsMessage = screen.queryByText(/no recommendations yet/i);
+  expect(NoRecommendationsMessage).not.toBeInTheDocument();
+
+  const carouselCards = screen.getAllByRole('link', { className: /card/i });
+  // at least 1 card for favourites, 1 for recommendations
+  expect(carouselCards.length).toBeGreaterThanOrEqual(2);
 });
