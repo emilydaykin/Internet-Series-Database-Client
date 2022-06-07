7ecfed8af06ccd15a61e0928d122c21e9a124309
Emily Daykin <emilydaykin31@gmail.com>
2022-06-07 22:33:21 +0200

    updated ElasticCarousel component

--- src/components/ElasticCarousel.js
+++ src/components/ElasticCarousel.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Carousel from 'react-elastic-carousel';
 import { Link } from 'react-router-dom';
 
-const ElasticCarousel = ({ seriesList }) => {
+const ElasticCarousel = ({ seriesList, listType }) => {
   const responsiveBreakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 2, itemsToScroll: 2 },
     { width: 768, itemsToShow: 3 },
     { width: 1200, itemsToShow: 4 }
   ];
+  // console.log('seriesList FROM INSIDE ElasticCAROUSEL: ------ ', seriesList);
+  // console.log('listType FROM INSIDE ElasticCAROUSEL: ------ ', listType);
 
   return (
     <div className='carousel'>
-      {seriesList ? (
+      {seriesList && seriesList.length > 0 ? (
         <Carousel breakPoints={responsiveBreakPoints}>
           {seriesList.map((series) => (
-            <Link key={series._id} to={`/series/${series._id}`} className='card'>
+            <Link
+              key={series._id}
+              to={`/series/${series._id}`}
+              className={`card card--${listType}`}
+            >
               <div className='card__side card__side--front'>
                 <img src={series.image} alt={series.name} className='carousel__image' />
               </div>
@@ -36,7 +42,11 @@ const ElasticCarousel = ({ seriesList }) => {
           ))}
         </Carousel>
       ) : (
-        <p>No favourites yet.</p>
+        <p>
+          {listType === 'favourites'
+            ? 'No favourites yet.'
+            : 'No recommendations yet. Add some series to your Favourites list get some recommendations!'}
+        </p>
       )}
     </div>
   );
