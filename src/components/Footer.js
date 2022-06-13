393c990721c080ffe6c5429803bfaea8ce104aee
Emily Daykin <emilydaykin31@gmail.com>
2022-06-13 13:39:02 +0200

    improved footer responsive design for small screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,22 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='footer__text'>
-        Designed, Created and Built by&nbsp;
-        <div className='footer__break-half'>
-          <a className='footer__linkedin' href='https://www.linkedin.com/in/emily-daykin/'>
-            Emily Daykin
-          </a>
-          &nbsp;
-          <a href='https://github.com/emilydaykin'>
-            <img src={githubLogo} alt='github' className='footer__github' />
+        <div className='footer__break footer__break--one'>
+          Designed, Created and Built by&nbsp;
+          <div className='footer__break-one-subset'>
+            <a className='footer__linkedin' href='https://www.linkedin.com/in/emily-daykin/'>
+              Emily Daykin
+            </a>
             &nbsp;
-          </a>
-          &#126; Copyright &copy; 2022
+            <a href='https://github.com/emilydaykin'>
+              <img src={githubLogo} alt='github' className='footer__github' />
+            </a>
+          </div>
         </div>
+        <div className='footer__break footer__break--two'>
+          &nbsp;in React.js, Node, Express and MongoDB&nbsp;
+        </div>
+        <div className='footer__break footer__break--three'>&#126; Copyright &copy; 2022</div>
       </div>
     </footer>
   );
